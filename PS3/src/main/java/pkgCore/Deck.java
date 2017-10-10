5ee37cb4ebe69f4e4d18b724fc1f6b0a58aba230
rongsong <rongsong@wifi-roaming-128-4-132-139.host.udel.edu>
2017-10-10 15:53:00 -0400

    Final Commit

--- PS3/src/main/java/pkgCore/Deck.java
+++ PS3/src/main/java/pkgCore/Deck.java
@@ -2,36 +2,79 @@ package pkgCore;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.*;
 
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
 
+import pkgException.DeckException;
+
 public class Deck {
+	
 
 	private ArrayList<Card> cardsInDeck;
 
 	public Deck() {
+		cardsInDeck = new ArrayList<Card>();
 		for (eSuit eSuit : eSuit.values()) {
 			for (eRank eRank : eRank.values()) {
-				cardsInDeck.add(new Card(eSuit, eRank));
+				this.cardsInDeck.add(new Card(eSuit, eRank));
 			}
 		}
 		Collections.shuffle(cardsInDeck);
 	}
 
-	//TODO: Fix the Draw method so it throws an exception if the deck is empty
-	public Card Draw() {
+	public Card Draw() throws DeckException {
+		try {
+			if (cardsInDeck.size() == 0) {
+				throw new DeckException(this);
+			}
+		} catch (DeckException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw e;
+		}
 		return cardsInDeck.remove(0);
 	}
-	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eSuit
-	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eRank
 
-	//TODO: Write a method that will return the number of a given eSuit left in the deck.
-	
-	//TODO: Write a method that will return the number of a given eRank left in the deck.
-	
-	//TODO: Write a method that will return 0 or 1 if a given card is left in the deck.
-	
-}
+
+	public Card Draw(eSuit eSuit) {
+		for (Card c : cardsInDeck) {
+			if (c.geteSuit() == eSuit) {
+				cardsInDeck.remove(c);
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public Card Draw(eRank eRank) {
+		for (Card c : cardsInDeck) {
+			if (c.geteRank() == eRank) {
+				cardsInDeck.remove(c);
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public int counteSuit(eSuit eSuit) {
+		List<Card> counteSuit = cardsInDeck.stream().filter(c -> c.geteSuit() == eSuit).collect(Collectors.toList());
+		return counteSuit.size();
+
+	}
+
+	public int counteRank(eRank eRank) {
+		List<Card> counteRank  = cardsInDeck.stream().filter(c -> c.geteRank() == eRank).collect(Collectors.toList());
+		return counteRank.size();
+	}
+
+	public int cardLeft(eSuit eSuit, eRank eRank) {
+		List<Card> cardsLeft = cardsInDeck.stream().filter(c -> c.geteSuit() == eSuit && c.geteRank() == eRank)
+				.collect(Collectors.toList());
+				return cardsLeft.size();
+		}
+
+	}
--- PS3/src/test/java/pkgCore/DeckTest.java
+++ PS3/src/test/java/pkgCore/DeckTest.java
@@ -4,29 +4,56 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+import pkgException.DeckException;
+
 public class DeckTest {
 
 	@Test
-	public void TestEmptyDeck() {
-		//TODO: Build a deck, draw until you get a DeckException
+	public void TestEmptyDeck() throws DeckException {
+		for (int i = 1; i <= 52; i++) {
+			new Deck().Draw();
+		}
 	}
+
 	
 	@Test
 	public void TestDrawSuit() {
 		//TODO: Build a deck, test the Draw(eSuit) method
+		for (eSuit eSuit : eSuit.values()) {
+			assertTrue(new Deck().Draw(eSuit).geteSuit() == eSuit);
+		}
 	}
 	
 	@Test
-	public void TestDrawRank() {
-		//TODO: Build a deck, test the Draw(eRank) method
-	}
-	
-	public void TestDeckRankCount() {
-		//TODO: Build a deck, test the DeckRankCount method
-	}
-	
-	public void TestDeckSuitCount() {
-		//TODO: Build a deck, test the DeckSuitCount method
+	public void TestDrawRank( ) {
+		for (eRank eRank : eRank.values()) {
+			assertTrue(new Deck().Draw(eRank).geteRank() == eRank);
+		}
 	}
 
-}
+	@Test
+	public void TestDeckSuitCount() {
+		for (eSuit eSuit : eSuit.values()) {
+			int eSuitCount = new Deck().counteSuit(eSuit);
+			assertEquals(eSuitCount, 13);
+	}
+	}
+	@Test
+	public void TestDeckRankCount() {
+		for (eRank eRank:eRank.values()) {
+			int eRankCount = new Deck().counteRank(eRank);
+			assertEquals(eRankCount, 4);
+			}
+	}
+	
+	@Test
+	public void TestIsCardLeft() throws DeckException {
+		Deck d = new Deck();
+		for (int x = 1; x < 53; x++) {
+			Card c = d.Draw();
+			assertEquals(d.cardLeft(c.geteSuit(), c.geteRank()), 0);
+		}
+	}
+	}
